2d3026247262ea05ea576c15631f0453d0837d65
Alexander Kashpir <alexander@kashpir.com>
2026-09-24 15:05:43 +1200

    Comment code and add README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Serial to CAN transceiver
+
+## Features
+- Send character from serial in frames over CAN
+- Display to serial frames that arrive over CAN
+
+## Author
+Alexander Kashpir 
+
+## Credits
+- [Arduino MCP2515 CAN interface](https://github.com/autowp/arduino-mcp2515)
+
--- UART.ino
+++ UART.ino
@@ -3,8 +3,8 @@
 
 #define USER_LED PC13
 
-#define GROUND_CONTROL_CAN_ID (0x01)
-#define SPI_CS_PIN            (PA0)
+#define CAN_ID     (0x01)
+#define SPI_CS_PIN (PA0 )
 
 
 MCP2515 mcp2515(SPI_CS_PIN);
@@ -12,6 +12,11 @@ MCP2515 mcp2515(SPI_CS_PIN);
 struct can_frame tx, rx;
 
 
+/**
+ * Flash the user LED for a given duration
+ *
+ * @param duration The time, in milliseconds, to keep  the LED on for
+ */
 void
 flash_led(int duration)
 {
@@ -20,62 +25,80 @@ flash_led(int duration)
   digitalWrite(USER_LED, 0);
 }
 
-void setup() {
+/**
+ * Set-up serial, CAN peripheral, tx frame, and enable the user LED
+ */
+void
+setup(void)
+{
+  /* Set-up serial, timeout at 5s */
   Serial.setTimeout(5000);
   Serial.begin(9600);
 
-  /* Set tx frame id and clear it */
-  tx.can_id = GROUND_CONTROL_CAN_ID;
-  tx.can_dlc = 0;
-  for (int i = 0; i < sizeof(tx.data); i++) tx.data[i] = ' ';
-
-  pinMode(USER_LED, OUTPUT);
-
+  /* Set-up CAN peripheral */
   mcp2515.reset();
   mcp2515.setBitrate(CAN_125KBPS);
   mcp2515.setNormalOneShotMode();
+
+  /* Set tx frame id and clear it */
+  tx.can_id = CAN_ID;
+  tx.can_dlc = 0;
+  for (int i = 0; i < sizeof(tx.data); i++) tx.data[i] = ' ';
+
+  /* Enable user LED */
+  pinMode(USER_LED, OUTPUT);
 }
 
-void loop() {
-  char c;
-
-  if (Serial.available())
+/**
+ * Read and send up to 8 characters from serial over CAN bus, printing to serial the sent frames,
+ * and also print any received frames.
+ */
+void
+loop(void)
+{
+  /* Read in characters into a frame to transmit */
+  while (Serial.available())
   {
-    c = Serial.read();
+    char c = Serial.read();
     flash_led(100);
 
+    /* If a character is not an EOL marker, add it to the frame */
     if (c != '\r' && c != '\n')
     {
       tx.data[tx.can_dlc] = c;
       tx.can_dlc++;
     }
 
+    /* If an EOL character was read or the frame is full */
     if (
         (tx.can_dlc > 0) &&
         (c == '\r' || c == '\n' || tx.can_dlc == sizeof(tx.data))
        )
     {
-      /* e.g. > Transmitting |G C1 S| with reported |DLC:6| */
+      /* Inform transmission over serial, e.g. 
+       * "> Transmitting |G C1 S| with reported |DLC:6|" */
       Serial.print("> Transmitting |");
       for (int i = 0; i < sizeof(tx.data); i++) Serial.print((char)tx.data[i]);
       Serial.print("| with reported |DLC:");
       Serial.print(tx.can_dlc);
       Serial.println("|");
 
+      /* Send the frame */
       mcp2515.sendMessage(&tx);
 
-      for (int i = 0; i < sizeof(tx.data); i++) tx.data[i] = ' ';
+      /* Clear frame for next transmission */
+      for (int i = 0; i < tx.can_dlc; i++) tx.data[i] = ' ';
       tx.can_dlc = 0;
-      while (Serial.available()) Serial.read();
     }
   }
 
+  /* If a frame is received */
   if (mcp2515.readMessage(&rx) == MCP2515::ERROR_OK)
   {
     flash_led(50);
-    flash_led(50);
 
-    /* e.g. > Received: |ID:0xDC| |DLC:8| -> |S C1 001| */
+    /* Output it to serial, e.g.
+     * "> Received: |ID:0xDC| |DLC:8| -> |S C1 001|" */
     Serial.print("> Received: ");
     Serial.print("|ID:");
     Serial.print(rx.can_id, HEX);
@@ -89,5 +112,5 @@ void loop() {
     Serial.println("|");
   }
 
-  delay(100);
+  delay(20);
 }
